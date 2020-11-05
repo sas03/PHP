c7204f3a502ae8c1ceeeebba9a6073716c278d31
sas03 <34343058+sas03@users.noreply.github.com>
2020-11-05 22:13:26 +0100

    Update inscription.php

--- mini-twitter/mini-twitter/inscription.php
+++ mini-twitter/mini-twitter/inscription.php
@@ -27,44 +27,39 @@ if (!empty($_POST['pseudo'])) {
 <html>
 <body>
 <div>
+    <!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<div>
     <form method="post" action="inscription.php" enctype="multipart/form-data">
-	<fieldset><legend>Identifiants</legend>
-	<label for="pseudo">* Pseudo :</label>  <input name="pseudo" type="text" id="pseudo" /> (le pseudo doit contenir entre 3 et 15 caractères)<br />
-	<label for="password">* Mot de Passe :</label><input type="password" name="password" id="password" /><br />
-	<label for="confirm">* Confirmer le mot de passe :</label><input type="password" name="confirm" id="confirm" />
-	</fieldset>
-	<fieldset><legend>Contacts</legend>
-	<label for="email">* Votre adresse Mail :</label><input type="text" name="email" id="email" /><br />
-	<label for="msn">Votre adresse MSN :</label><input type="text" name="msn" id="msn" /><br />
-	<label for="website">Votre site web :</label><input type="text" name="website" id="website" />
-	</fieldset>
-	<fieldset><legend>Informations supplémentaires</legend>
-	<label for="localisation">Localisation :</label><input type="text" name="localisation" id="localisation" />
-	</fieldset>
-	<fieldset><legend>Profil sur le forum</legend>
-	<label for="avatar">Choisissez votre avatar :</label><input type="file" name="avatar" id="avatar" />(Taille max : 10Ko<br />
-	<label for="signature">Signature :</label><textarea cols="40" rows="4" name="signature" id="signature">La signature est limitée à 200 caractères</textarea>
-	</fieldset>
-	<p>Les champs précédés d un * sont obligatoires</p>
-	<p><input type="submit" value="S'inscrire" /></p></form>
+        <fieldset><legend>Identifiants</legend>
+        <label for="pseudo">* Pseudo :</label>  <input name="pseudo" type="text" id="pseudo" /><br />
+        <label for="password">* Mot de Passe :</label><input type="password" name="password" id="password" /><br />
+        <label for="confirm">* Confirmer le mot de passe :</label><input type="password" name="confirm" id="confirm" />
+        </fieldset>
+        <p><input type="submit" value="S'inscrire" /></p>
+    </form>
 </div>
 Déja inscrit? <a href="connection.php">Se Connecter</a>
-</body>
+
+<?php
+
+if(isset($_POST['pseudo']) && ($_POST['password'] == $_POST['confirm']))
+{
+    $req = $dbh->prepare('INSERT INTO users (pseudo, pass) VALUES (?,?)');
+    $req->execute([$_POST['pseudo'],$_POST['password']]);
+    
+    header("Location: index.php");
+}
 <footer>
     <br>
     <small>Copyrights 2017</small>
 </footer>
+</body>
 </html>
-<?php
-}
-?>
 
-<?php
-
-
-
-?>
-?>
 <?php
 /*if (empty($_POST['pseudo'])){
     echo '<h1>Inscription 1/2</h1>';
